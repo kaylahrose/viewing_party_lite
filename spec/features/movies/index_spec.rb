176ff032b3e3380ce8fc499fa6cd469e0c20eff8
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-31 19:07:34 -0800

    Create discover movies button path

---
+++ spec/features/movies/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'movies index page' do
+#   As an user,
+# When I visit the '/users/:id/discover' path, where :id, is the id of a valid user,
+# I should see
+
+#  Button to Discover Top Rated Movies
+#  A text field to enter keyword(s) to search by movie title
+#  A Button to Search by Movie Title
+# Details When the user clicks on the Top Rated Movies OR the search button,
+# they should be taken to the movies results page (more details of this on the Movies Results Page issue.
+
+# The movies will be retrieved by consuming The MovieDB API
+end
