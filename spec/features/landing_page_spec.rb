a5f008c83884c6dcfbaa676eaa3ec6971d31d639
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-14 10:43:20 -0800

    update landing page test expectations when user logged in vs logged out

--- spec/features/landing_page_spec.rb
+++ spec/features/landing_page_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'landing page' do
       expect(page).to have_content 'Register New User'
     end
 
-    it 'has links of all existing users' do
+    it 'has emails of all existing users when user is logged in' do
       charlie = User.create!(name: 'Charlie', email: 'charlie_boy@gmail.com', password: 'password123', password_confirmation: 'password123')
       nicole = User.create!(name: 'Nicole', email: 'nicoley_oley@yahoo.com', password: 'sosecure123', password_confirmation: 'sosecure123')
       sara = User.create!(name: 'Sara', email: 'sara1983@gmail.com', password: 'password', password_confirmation: 'password')
@@ -31,9 +31,25 @@ RSpec.describe 'landing page' do
       visit root_path
 
       within '#users' do
+        expect(page).to have_content("Existing Users")
         expect(page).to have_content(charlie.email)
         expect(page).to have_content(nicole.email)
         expect(page).to have_content(sara.email)
+      end
+    end
+
+    it 'does not show other user emails if not logged in' do 
+      charlie = User.create!(name: 'Charlie', email: 'charlie_boy@gmail.com', password: 'password123', password_confirmation: 'password123')
+      nicole = User.create!(name: 'Nicole', email: 'nicoley_oley@yahoo.com', password: 'sosecure123', password_confirmation: 'sosecure123')
+      sara = User.create!(name: 'Sara', email: 'sara1983@gmail.com', password: 'password', password_confirmation: 'password')
+
+      visit root_path
+
+      within '#users' do
+        expect(page).to_not have_content("Existing Users")
+        expect(page).to_not have_content(charlie.email)
+        expect(page).to_not have_content(nicole.email)
+        expect(page).to_not have_content(sara.email)
       end
     end
 
