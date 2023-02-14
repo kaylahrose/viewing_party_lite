c54db0c5ed3365fecf27e3db84a8e5747bb0c4e1
Kerynn Davis <kerynn.davis@gmail.com>
2023-02-14 12:23:13 -0800

    update viewing party new spec to have logged in users

--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -18,6 +18,12 @@ RSpec.describe 'The new viewing party page' do
 
   describe 'happy path' do
     it 'creates a new viewing party with default duration and no other users' do
+      visit login_path
+
+      fill_in 'email', with: 'charlie_boy@gmail.com'
+      fill_in 'password', with: 'password123'
+      click_button "Log In"
+
       visit "/users/#{charlie.id}/movies/497/viewing-party/new"
       expect(ViewingParty.all).to eq([])
       within('#form') do
@@ -27,42 +33,67 @@ RSpec.describe 'The new viewing party page' do
       end
       expect(ViewingParty.all.count).to eq(1)
       expect(ViewingParty.all[0].host_id).to eq(charlie.id)
-      expect(current_path).to eq("/users/#{charlie.id}")
+      expect(current_path).to eq(user_path(charlie))
     end
 
     it 'creates viewing party with custom duration and no other users' do
+      visit login_path
+
+      fill_in 'email', with: 'charlie_boy@gmail.com'
+      fill_in 'password', with: 'password123'
+      click_button "Log In"
+
       visit "/users/#{charlie.id}/movies/497/viewing-party/new"
+      
       within('#form') do
         fill_in 'duration', with: '200'
         fill_in 'day', with: Date.tomorrow
         fill_in 'time', with: Time.now
         click_button 'Create Party'
       end
-      expect(current_path).to eq("/users/#{charlie.id}")
+
+      expect(current_path).to eq(user_path(charlie))
     end
 
     it 'creates viewing party with default duration and other users' do
+      visit login_path
+
+      fill_in 'email', with: 'charlie_boy@gmail.com'
+      fill_in 'password', with: 'password123'
+      click_button "Log In"
+
       visit "/users/#{charlie.id}/movies/497/viewing-party/new"
+      
       within('#form') do
         fill_in 'day', with: Date.tomorrow
         fill_in 'time', with: Time.now
         check cindy.name
         click_button 'Create Party'
       end
-      expect(current_path).to eq("/users/#{charlie.id}")
+
+      expect(current_path).to eq(user_path(charlie))
       expect(cindy.user_viewing_parties.count).to eq(1)
       expect(UserViewingParty.all.count).to eq 2
     end
 
     it 'shows up on other users dashboards (show pages)' do
+      visit login_path
+
+      fill_in 'email', with: 'user@gmail.com'
+      fill_in 'password', with: 'sosecure123'
+      click_button "Log In"
+
       visit "/users/#{charlie.id}/movies/497/viewing-party/new"
+      
       within('#form') do
         fill_in 'day', with: Date.tomorrow
         fill_in 'time', with: Time.now
         check cindy.name
         click_button 'Create Party'
       end
-      visit "/users/#{cindy.id}"
+
+      visit user_path(cindy)
+
       within '#viewing_parties' do
         expect(page).to have_content(cindy.viewing_parties[0].event_date.strftime('%B %-d, %Y'))
       end
@@ -71,14 +102,18 @@ RSpec.describe 'The new viewing party page' do
 
   describe 'sad path' do
     it 'does not create viewing party with duration less than movie runtime' do
+      
       visit "/users/#{charlie.id}/movies/497/viewing-party/new"
+      
       expect(ViewingParty.all).to eq([])
+      
       within('#form') do
         fill_in 'duration', with: 0
         fill_in 'day', with: Date.tomorrow
         fill_in 'time', with: Time.now
         click_button 'Create Party'
       end
+      
       expect(current_path).to eq("/users/#{charlie.id}/movies/497/viewing-party/new")
       expect(page).to have_content('Party time must be longer than the movie runtime.')
       expect(ViewingParty.all).to eq([])
